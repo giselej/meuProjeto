913595edec784853b14442088b7e60d0ce290784
Your Name <giselej.albuquerque@gmail.com>
2023-08-07 22:24:28 -0400

    models

---
+++ database/migrations/2023_08_08_015915_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->text('conteudo');
+            $table->unsignedBiglnteger('usuario_id');//chave estrageira para usuarios
+            $table->timestamps();
+            $table->foreign('usuario_id')->references('id')->on('usuarios')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
