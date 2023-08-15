e2284b7d76b71d7ade5005a7527de46e8b4777e5
unknown <giselej.albuquerque@gmail.com>
2023-08-14 22:25:05 -0400

    seeder

---
+++ database/seeders/PostsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Usuario;
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = Usuario::first();
+
+        Post::create([
+            'usuario_id'=>$user->id,
+            'titulo'=>'Post de exemplo',
+            'conteudo'=> 'Este é o corpo do post de exemplo.',
+        ]);
+    }
+}
---
+++ database/seeders/UsuariosTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Database\Seeders;
+
+use App\Models\Usuario;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsuariosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Usuario::crete([
+            'nome'=>'joao Silva',
+            'email'=>'joao.silva@email.com',
+
+        ]);
+    }
+}
